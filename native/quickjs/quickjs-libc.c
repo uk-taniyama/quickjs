dc5232a16b96a50fbbe1b9193b9e7e889c49defb
WebFolder OÜ <support@webfolder.io>
2020-07-10 21:23:31 +0300

    [build linux]

--- native/quickjs/quickjs-libc.c
+++ native/quickjs/quickjs-libc.c
@@ -57,6 +57,10 @@ typedef sig_t sighandler_t;
 
 #endif
 
+#ifdef __linux__ 
+extern char** environ;
+#endif
+
 #if !defined(_WIN32)
 /* enable the os.Worker API. IT relies on POSIX threads */
 #define USE_WORKER
