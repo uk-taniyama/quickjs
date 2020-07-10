f80bc72a05dcdc78c4f286505b04936b6fdb65d6
webfolderio <support@webfolder.io>
2020-07-10 19:46:16 +0300

    [build macos]

--- native/build-macos.sh
+++ native/build-macos.sh
@@ -1,2 +1,2 @@
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang -DCMAKE_BUILD_TYPE=Release .. -GNinja
-ninja
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang -DCMAKE_BUILD_TYPE=Release ..
+make
